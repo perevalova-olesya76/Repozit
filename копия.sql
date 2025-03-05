0713ce6f9072c0ab91d36d0c3830a4a3fc6ba77d
perevalova-olesya76 <olesyaperevalova2006@gmail.com>
2025-03-05 15:28:30 +0700

    Update копия.sql

--- копия.sql
+++ копия.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `appointments`
 --
 
-DROP TABLE IF EXISTS `appointments`;
+DROP TABLE IF EXISTS `appointmentssssss`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `appointmentss` (
