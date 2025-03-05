0f0196a4b12a2e3f324c646f5ba1e27540156438
perevalova-olesya76 <olesyaperevalova2006@gmail.com>
2025-03-05 15:20:44 +0700

    Update копия.sql

--- копия.sql
+++ копия.sql
@@ -22,7 +22,7 @@
 DROP TABLE IF EXISTS `appointments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `appointments` (
+CREATE TABLE `appointmentsss` (
   `id` int NOT NULL AUTO_INCREMENT,
   `Fio` varchar(255) NOT NULL,
   `appIspol` varchar(255) NOT NULL,
